1f6c9db7cd2d4f042b62947aeabaad037251ce12
Adam Horrigan <adhorrig@gmail.com>
2015-10-01 10:49:44 +0100

    Modified file paths
    All folders are now back a level

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new mysqli("localhost", "adam", "password", "easyfix");
+	$mysqli = new mysqli("localhost", "root", "password", "easyfix");
 	if($mysqli->connect_errno){
 		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
