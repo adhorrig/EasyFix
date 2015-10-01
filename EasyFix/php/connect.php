5b4468ed962f739a2bf9800122f5db0d7a4159ac
Adam Horrigan <adhorrig@gmail.com>
2015-10-01 09:39:32 +0100

    Insert to database
    Inset registration variables to the database

--- EasyFix/php/connect.php
+++ EasyFix/php/connect.php
@@ -1,12 +1,7 @@
 <?php
-	$servername="localhost";
-	$username="easyfix"
-	$password="easyfix";
-	
-	$connect=new mysqli($servername, $username, $password);
-	
-	if($connect->connect_error){
-		die("Connection failed " . $connect->connect_error);
+	$mysqli = new mysqli("localhost", "adam", "password", "easyfix");
+	if($mysqli->connect_errno){
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
 	echo "Connected successfully"
 ?>
--- EasyFix/php/registration.php
+++ EasyFix/php/registration.php
@@ -1,6 +1,13 @@
 <?php
+	require 'connect.php';
 	$username = $_POST["Username"];
 	$email = $_POST["Email"];
 	$password = $_POST["Password"];
 	$cpassword = $_POST["Cpassword"];
+	
+	$query = "INSERT INTO users VALUES (NUll, $username, $email, $password, $cpassword)";
+	$mysqli->query($query);
+	
+	$mysqli->close();
+	?>
 ?>
