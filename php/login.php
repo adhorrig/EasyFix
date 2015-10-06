c7154cf4387b86913703324409cf1dbd2199f619
ADHORRIG <adhorrig@gmail.com>
2015-10-06 19:46:58 +0100

    Login variables posted to PHP
    Can now check against the username and password in the database, to
    allow the user to login or not.

---
+++ php/login.php
@@ -0,0 +1,14 @@
+<?php
+	require 'connect.php';
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+
+	echo $username;
+	echo $password;
+	
+	//or die(mysqli_error($db));
+
+ 
+	
+
+?>
