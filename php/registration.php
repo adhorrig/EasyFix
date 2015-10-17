1188663ca6a208a423a378919d17f4d6eb968548
ADHORRIG <adhorrig@gmail.com>
2015-10-17 21:01:27 +0100

    Account type inserted to user table

--- php/registration.php
+++ php/registration.php
@@ -4,14 +4,15 @@
 	$email = $_POST["Email"];
 	$password = $_POST["Password"];
 	$cpassword = $_POST["Cpassword"];
+	$accounttype = $_POST["Actype"];
 	
 	//echo $username;
 	//echo $email;
 	//echo $password;
 	//echo $cpassword;
 	
-	mysqli_query($db,"INSERT INTO users (username, email, password)
-	VALUES ('". $username ."', '". $email ."', '". $password ."')") 
+	mysqli_query($db,"INSERT INTO users (username, email, password, AccountType)
+	VALUES ('". $username ."', '". $email ."', '". $password ."', '". $accounttype ."')") 
 	or die(mysqli_error($db));
 	include('../html/login.html');
  
