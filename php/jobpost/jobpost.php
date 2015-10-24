53839fbc2171fa1fa3d0e27e61929c4b1320eecf
ADHORRIG <adhorrig@gmail.com>
2015-10-24 23:13:45 +0100

    Insert username of job poster
    Allow us to see which user posted what job.

--- php/jobpost/jobpost.php
+++ php/jobpost/jobpost.php
@@ -1,5 +1,7 @@
 <?php
 require '../connect.php';
+session_start();
+$_SESSION['username'];
 $description = $_POST["comment"];
 $price = $_POST["price"];
 $urgency = $_POST["urgency"];
@@ -8,8 +10,8 @@ $urgency = $_POST["urgency"];
 echo $price;
 echo $urgency;*/
 
-mysqli_query($db,"INSERT INTO jobs (description, price, urgency)
-	VALUES ('". $description ."', '". $price ."', '". $urgency ."')")
+mysqli_query($db,"INSERT INTO jobs (description, price, urgency, username)
+	VALUES ('". $description ."', '". $price ."', '". $urgency ."', '". $_SESSION['username']. "')")
 	or die(mysqli_error($db));
 	include('JobPostConfirm.php');
 
