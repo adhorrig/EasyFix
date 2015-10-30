25f88e53a5566f957a37bacc1e06a18f16a90ffb
Adam Horrigan <adhorrig@gmail.com>
2015-10-30 16:13:53 +0000

    Redirect based on accounttype

--- php/registration/login.php
+++ php/registration/login.php
@@ -5,23 +5,28 @@
 	$password = $_POST["password"];
 	$_SESSION['username'] = $username;
 
-	//echo $username;
-	//echo $password;
+	$sql = "SELECT AccountType, password, username FROM users WHERE password = '". $password ."' AND username = '". $username ."'";
+	$result = $db->query($sql);
 
-	//Possible solution for logging a user in below.
-	if($result = mysqli_query($db,"SELECT password FROM USERS WHERE password = '". $password ."' AND username = '". $username ."'")){
-		$count = $result->num_rows;
-		if($count>=1){
-			include('../welcomepages/WelcomeCustomer.php');
-
-		}
-		else{
-			include('../registration/login.php');
-			echo '<script language="javascript">';
-			echo 'alert("Username or password was entered incorrectly")';
-			echo '</script>';
-
-		}
+	if($result->num_rows){
+			while($row = $result->fetch_assoc()){
+					$count = $result->num_rows;
+					$accounttype = $row["AccountType"];
+					if($count>=1){
+							if($accounttype == "Customer"){
+									include('../welcomepages/WelcomeCustomer.php');
+							}
+							else if($accounttype = "Tradesmen"){
+									include('../welcomepages/WelcomeTrade.php');
+							}
+					}
+					else{
+							include('../registration/login.php');
+							echo '<script language="javascript">';
+							echo 'alert("Username or password was entered incorrectly")';
+							echo '</script>';
+					}
+			}
 	}
 
 ?>
