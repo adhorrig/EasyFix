ca37c2b41156d8bc2176bf8302ba2374b0e17807
Adam Horrigan <adhorrig@gmail.com>
2015-11-10 22:31:14 +0000

    Set up availability pages

---
+++ php/active/setactive.php
@@ -0,0 +1,72 @@
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <title>Welcome</title>
+        <style type="text/css">
+            body{
+            background-color: #E8E8E8;
+            }
+            .other-color{
+            background-color:#c0c0c0;
+            }
+        </style>
+    </head>
+    <body>
+        <div class = "container">
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="">EasyFix</a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="WelcomeCustomer.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="../jobpost/jobpostView.php">Post a job</a>
+                        </li>
+                        <li>
+                            <a href="#">Set your availability</a>
+                        </li>
+                        <li>
+                            <a href="../profile/profile.php">Your profile</a>
+                        </li>
+                        <li>
+                            <a href="../session/destroy.php">Logout</a>
+                        </li>
+                    </div>
+                    <!--/.nav-collapse -->
+                </div>
+                <!--/.container-fluid -->
+            </nav>
+            <div class="jumbotron other-color">
+                <h4> Set your active status below </h4>
+                <form action="success.php" method="post" name="availability" class="form-signin" >
+                  <select class="form-control">
+                      <option name = "active">I am available</option>
+                      <option name = "notActive">I am not available.</option>
+                  </select>
+                  </br>
+                  <div class="btn-toobar">
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
