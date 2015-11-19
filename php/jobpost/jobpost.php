0d535f931d2c367868c3ba77da23ba13a8e8327b
Adam Horrigan <adhorrig@gmail.com>
2015-11-19 10:08:51 +0000

    input type = text -> input type = select

--- php/jobpost/jobpost.php
+++ php/jobpost/jobpost.php
@@ -1,25 +1,25 @@
 <?php
-		require '../connect.php';
-		session_start();
-		$_SESSION['username'];
-		$description = $_POST["comment"];
-		$price = $_POST["price"];
-		$urgency = $_POST["urgency"];
-		$file_size = $_FILES['image']['size'];
-		$file_tmp = $_FILES['image']['tmp_name'];
-    $uniqname = uniqid() . '.jpg';
-    $file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
-    $allowedExtensions = array("jpeg", "jpg", "png");
-    if (in_array($file_ext, $allowedExtensions) === false) {
-        die("extension not allowed, please choose a JPEG or PNG file.");
-    }
-    if ($file_size > 1048576) {
-        die('File size can not be greater than 1mb');
-    } else {
-        move_uploaded_file($file_tmp, "../../images/" . $uniqname);
-				mysqli_query($db,"INSERT INTO jobs (description, price, urgency, username, photo)
-				VALUES ('". $description ."', '". $price ."', '". $urgency ."', '". $_SESSION['username']. "', '". $uniqname . "')")
-				or die(mysqli_error($db));
-				include('JobPostConfirm.php');
-    }
+	require '../connect.php';
+	session_start();
+	$_SESSION['username'];
+	$description = $_POST["comment"];
+	$price = $_POST["price"];
+	$urgency = $_POST["urgency"];
+	$file_size = $_FILES['image']['size'];
+	$file_tmp = $_FILES['image']['tmp_name'];
+	$uniqname = uniqid() . '.jpg';
+	$file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
+	$allowedExtensions = array("jpeg", "jpg", "png");
+	if (in_array($file_ext, $allowedExtensions) === false) {
+		die("extension not allowed, please choose a JPEG or PNG file.");
+	}
+	if ($file_size > 1048576) {
+		die('File size can not be greater than 1mb');
+	} else {
+		move_uploaded_file($file_tmp, "../../images/" . $uniqname);
+			mysqli_query($db,"INSERT INTO jobs (description, price, urgency, username, photo)
+			VALUES ('". $description ."', '". $price ."', '". $urgency ."', '". $_SESSION['username']. "', '". $uniqname . "')")
+			or die(mysqli_error($db));
+			include('JobPostConfirm.php');
+	}
 ?>
